6f7caa2206dc8a0863490d419e290b70fdab88c4
akshay <akshay@akshay-VirtualBox.(none)>
2013-01-02 13:23:53 +0530

    Akshay/MJ : Making the connection profile return profile instead of user

--- bednets/models.py
+++ bednets/models.py
@@ -1,8 +1,12 @@
 from rapidsms.models import Contact
 from bednets.spreadsheets_utils import is_empty
+
 class ConnectionProfile(object):
+
+    def __init__(self, user):
+        self.user = user
 
     @classmethod
     def lookup_by_connection(cls, connection):
         contact = Contact.objects.filter(connection=connection)
-        return None if is_empty(contact) else contact[0].user
+        return None if is_empty(contact) else ConnectionProfile(contact[0].user)
--- bednets/tests/integration/test_connection_profile.py
+++ bednets/tests/integration/test_connection_profile.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from django.contrib.auth.models import User
 from rapidsms.models import Contact, Connection, Backend
 from bednets.models import ConnectionProfile
+from mock import Mock
 
 class TestConnectionProfile(TestCase):
     def test_should_return_user_for_connection(self):
@@ -19,8 +20,14 @@ class TestConnectionProfile(TestCase):
         connection1.contact = contact
         connection1.save()
 
-        self.assertEqual(user, ConnectionProfile.lookup_by_connection(connection1))
+        connection_profile = ConnectionProfile.lookup_by_connection(connection1)
+        self.assertEqual(user, connection_profile.user)
 
     def test_should_return_none_if_no_user_for_connection(self):
         self.assertIsNone(ConnectionProfile.lookup_by_connection(Connection()))
+
+    def test_should_create_profile_with_user(self):
+        user = Mock(spec=User)
+        connection_profile = ConnectionProfile(user)
+        self.assertEqual(user, connection_profile.user)
 
