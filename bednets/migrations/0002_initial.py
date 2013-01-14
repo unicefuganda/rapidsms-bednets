ad007f9c25c8673c5e6870f5d5ecce683ba50afb
Argha C <007.argha@gmail.com>
2013-01-14 18:46:03 +0530

    Argha : bednets report signal handler for each xform submission on sent,recv,dist

---
+++ bednets/migrations/0002_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'BednetsReport'
+        db.create_table('bednets_bednetsreport', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('sub_county', self.gf('django.db.models.fields.TextField')()),
+            ('distribution_point', self.gf('django.db.models.fields.TextField')()),
+            ('quantity_at_subcounty', self.gf('django.db.models.fields.IntegerField')(default=0, null=True)),
+            ('quantity_sent_to_dp', self.gf('django.db.models.fields.IntegerField')(default=0, null=True)),
+            ('quantity_received_at_dp', self.gf('django.db.models.fields.IntegerField')(default=0, null=True)),
+            ('quantity_distributed_at_dp', self.gf('django.db.models.fields.IntegerField')(default=0, null=True)),
+        ))
+        db.send_create_signal('bednets', ['BednetsReport'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'BednetsReport'
+        db.delete_table('bednets_bednetsreport')
+
+
+    models = {
+        'bednets.bednetsreport': {
+            'Meta': {'object_name': 'BednetsReport'},
+            'distribution_point': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'quantity_at_subcounty': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'quantity_distributed_at_dp': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'quantity_received_at_dp': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'quantity_sent_to_dp': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'sub_county': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['bednets']
--- bednets/models.py
+++ bednets/models.py
@@ -1,6 +1,10 @@
 from rapidsms.models import Contact
 from bednets.spreadsheets_utils import is_empty
-
+from django.db import models
+#from bednets.signals import *
+from django.core.signals import request_finished
+from django.dispatch import receiver
+from rapidsms_xforms.models import xform_received
 class ConnectionProfile(object):
 
     def __init__(self, user):
@@ -10,3 +14,87 @@ class ConnectionProfile(object):
     def lookup_by_connection(cls, connection):
         contact = Contact.objects.filter(connection=connection)
         return None if is_empty(contact) else ConnectionProfile(contact[0].user)
+
+class BednetsReport(models.Model):
+    sub_county = models.TextField()
+    distribution_point = models.TextField()
+    quantity_at_subcounty = models.IntegerField(null = True,default=0)
+    quantity_sent_to_dp = models.IntegerField(null=True,default=0)
+    quantity_received_at_dp = models.IntegerField(null=True,default=0)
+    quantity_distributed_at_dp = models.IntegerField(null=True,default=0)
+
+
+bednets = ['send', 'recv', 'dist']
+@receiver(xform_received)
+
+def handle_submission(sender, **kwargs):
+    xform = kwargs['xform']
+
+    if xform.keyword not in bednets:
+        return
+    submission = kwargs['submission']
+    update_reports(xform,submission)
+
+def update_reports(xform,submission):
+    if submission.has_errors:
+        return
+    if xform.keyword == "send":
+        try:
+            sub_county = submission.eav_values.all()[1].value
+            distribution_point = submission.eav_values.all()[2].value
+            quantity_sent_to_dp = submission.eav_values.all()[0].value
+            existing_records = BednetsReport.objects.filter(sub_county=sub_county)
+            for record in existing_records:
+                if record.distribution_point == distribution_point:
+                    record.quantity_sent_to_dp += quantity_sent_to_dp
+                    record.save()
+                    return
+            BednetsReport.objects.create(sub_county=sub_county,distribution_point=distribution_point,quantity_sent_to_dp=quantity_sent_to_dp)
+        except Exception as e:
+            print str(e)
+        return
+
+    elif xform.keyword == "recv":
+        try:
+            received_at = submission.eav_values.all()[1].value #sc or dp
+            quantity_received = submission.eav_values.all()[0].value
+
+            #if it was received at a DP
+            dp_exists = BednetsReport.objects.filter(distribution_point=received_at) #should only be 1 entry for each DP
+            if len(dp_exists) == 1:
+                dp_exists[0].quantity_received_at_dp += quantity_received
+                dp_exists[0].save()
+                return
+
+            elif len(dp_exists) == 0:
+                subcounty_exists = BednetsReport.objects.filter(sub_county=received_at)
+                for subcounty in subcounty_exists:
+                    if subcounty.distribution_point == '':
+                        subcounty.quantity_at_subcounty += quantity_received
+                        subcounty.save()
+                        return
+
+            BednetsReport.objects.create(sub_county=received_at,quantity_at_subcounty=quantity_received)
+
+        except Exception as e:
+            print str(e)
+        return
+
+    elif xform.keyword == "dist":
+        try:
+            distributed_at = submission.eav_values.all()[1].value
+            quantity_distributed = submission.eav_values.all()[0].value
+
+            dp_exists = BednetsReport.objects.filter(distribution_point=distributed_at) #should only be 1 entry for each DP
+            if len(dp_exists) == 1:
+                dp_exists[0].quantity_distributed_at_dp += quantity_distributed
+                dp_exists[0].save()
+                return
+
+            elif len(dp_exists) == 0:
+                BednetsReport.objects.create(distribution_point=distributed_at,quantity_distributed_at_dp=quantity_distributed)
+                return
+
+        except Exception as e:
+            print str(e)
+        return
