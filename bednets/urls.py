6d5c44e8c6b418d5680c5c681aaa5852c0042c19
Kenneth <matovken@gmail.com>
2013-01-21 13:02:12 +0300

    applying authentication

--- bednets/urls.py
+++ bednets/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 from bednets.views import generate_bednets_report, generate_dump_bednets_report
 
 urlpatterns = patterns('',
-    url(r'^$',direct_to_template,{'template':'bednets/bednets.html'},name='bednets'),
+    url(r'^$',login_required(direct_to_template),{'template':'bednets/bednets.html'},name='bednets'),
     url(r'^report/$',generate_bednets_report ,name="bednets_excel_report"),
     url(r'^dump/$',generate_dump_bednets_report ,name="bednets_data_dump"),
 )
--- bednets/views.py
+++ bednets/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from mtrack_project.rapidsms_bednets.bednets.view_helpers import generate_excel_response,generate_multiple_excel_sheets_response,\
      get_data_dump, get_consolidated_data
 
 
+@login_required
 def generate_bednets_report(request):
     data = get_consolidated_data()
     headings = ["Sub-County","Quantity Received at Sub-County" ,"Quantity sent to Distribution Point", "Distribution Point", "Quantity Received at Distribution Point", "Quantity Distributed at Distribution Point" ,"Quantity Received at DP - Quantity Distributed at DP"]
     return generate_excel_response(data, headings)
 
+@login_required
 def generate_dump_bednets_report(request):
     sent_data = get_data_dump(keyword="send")
     received_data = get_data_dump(keyword="recv")
